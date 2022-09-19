7ae479f199cc9007534f745f249cd774e6d099bc
Camilulah <camilavd045@gmail.com>
2022-09-19 12:44:34 -0300

    Tenia que editar lo de (servido andando) porque me daba toc XD

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-//9pongo publica la carpeta public
+//Pongo publica la carpeta public
 app.use(express.static(path.join(__dirname, "/public")))
 
 
-//poner en servidor localhost:3000
+//Poner en servidor localhost:3000
 app.listen(3000,function(){
-    console.log("servido andando")
+    console.log("Servidor andando")
 })
 
-//poner en la web http://localhost:3000/home
+//Poner en la web http://localhost:3000/home
 app.get("/home",(req,res)=>{
     res.sendFile((__dirname + "/views/home.html"))
 
 })
 
-//poner en la web http://localhost:3000/login
+//Poner en la web http://localhost:3000/login
 app.get("/login",(req,res)=>{
     res.sendFile((__dirname + "/views/login.html"))
  
 })
 
-//poner en la web http://localhost:3000/registrarse
+//Poner en la web http://localhost:3000/registrarse
 app.get("/registrarse",(req,res)=>{
     res.sendFile((__dirname + "/views/registrarse.html"))
  
 })
 
-//poner en la web http://localhost:3000/descripcionproducto
+//Poner en la web http://localhost:3000/descripcionproducto
 app.get("/descripcionproducto",(req,res)=>{
     res.sendFile((__dirname + "/views/descripcionproducto.html"))
 
 })
-//poner en la web http://localhost:3000/carrocompras
+//Poner en la web http://localhost:3000/carrocompras
 app.get("/carrocompras",(req,res)=>{
     res.sendFile((__dirname + "/views/carrocompras.html"))
 
 })
 
-//rutas no asignadas   
+//Rutas no asignadas   
 app.get("*",(req,res)=>{//aca le paso la locaciona a donde redirecciono que es *
     res.send("rutas no estan asignadas")
 })
